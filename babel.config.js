c8f6ee3ecaf1ca0daaa1daa5f803bf999f09e9a2
cancelei <glauber.bannwart@gmail.com>
2023-11-08 16:41:07 -0300

    Add Linters and Configuration files

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    ['@babel/preset-env', { targets: { esmodules: true } }],
+    '@babel/preset-react'
+  ];
+  
+  const plugins = [];
+
+  return {
+    presets,
+    plugins
+  };
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,24 @@ const path    = require("path")
 const webpack = require("webpack")
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: "./app/javascript/packs/application.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
   },
   output: {
     filename: "[name].js",
